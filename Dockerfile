7b5bab27c9dbe55c6bd21941207eca7162d44d99
rubenpve <rverdugo@travix.com>
2020-05-23 19:58:20 +0100

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM alpine
+
+RUN apk add --no-cache curl bash && \
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl && \
+    chmod +x ./kubectl && \
+    mv ./kubectl /usr/local/bin/kubectl
+
+COPY entrypoint.sh /app/entrypoint.sh
+
+RUN chmod +x /app/entrypoint.sh
+
+ENV BACKUP_UID 1000
+ENV BACKUP_GID 999
+ENV SLEEP_SECONDS 30
+ENV BACKUP_NAME_PREFIX volumes-backup
+ENV BACKUPS_TO_KEEP 3
+ENV BACKUP_DIRECTORY /backup
+ENV PERSISTENT_VOLUMES_ROOT /persistentVolumes
+ENV BACKUP_IGNORE_CONFIG_FILE /config/dont-backup.txt
+
+VOLUME ["/backup", "/persistentVolumes", "/config"]
+
+ENTRYPOINT ["/app/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+#set -x
+set -e
+
+#Variables
+deployments=()
+notFound=0
+
+#TODO improve to get from the volumeclaim using jsonpath filters??
+get_deployment_names(){
+
+  dir_names=("${PERSISTENT_VOLUMES_ROOT}"/*)
+  for ((i=0; i<${#dir_names[@]}; i++)); do
+    foundPath="${dir_names[$i]}"
+    if [[ -d "$foundPath" ]]; then
+      deployment=$(basename "${foundPath}")
+      deployments+=("$deployment")
+    else
+      echo "${foundPath} is not a directory, ignoring"
+    fi
+  done
+}
+
+get_namespace(){
+  kubectl get deployment -A --field-selector metadata.name="$1" -o=jsonpath="{..metadata.namespace}"
+}
+
+#Scale deployments to 0
+stop_deployments(){
+    echo "###### Scaling down deployments ######"
+    for ((i=0; i<${#deployments[@]}; i++)); do
+        deployment="${deployments[$i]}"
+        namespace=$(get_namespace "$deployment")
+        kubectl scale deployment "$deployment" -n "$namespace" --replicas=0 || ((notFound++)) || true
+    done
+    echo "###### Finished scaling down deployments ######"
+}
+
+#TODO stop hardcoding 1 replica and use previous number
+#Scale deployments back up
+start_stopped_deployments(){
+    echo "###### Scaling deployments back up ######"
+    for ((i=0; i<${#deployments[@]}; i++)); do
+        deployment="${deployments[$i]}"
+        namespace=$(get_namespace "$deployment")
+        kubectl scale deployment "$deployment" -n "$namespace" --replicas=1 || true
+    done
+    echo "###### Finished scaling up deployments ######"
+}
+
+
+backup(){
+    echo "###### Backing up persistent volumes ######"
+    BACKUP_FILENAME="${BACKUP_DIRECTORY}/${BACKUP_NAME_PREFIX}_$(date +%F_%R).tar.gz"
+    if test -f "${BACKUP_IGNORE_CONFIG_FILE}"; then
+        echo "### Files that will not be backed up: ###"
+        cat "${BACKUP_IGNORE_CONFIG_FILE}"
+        EXCLUSION_ARGUMENT="--exclude-from=${BACKUP_IGNORE_CONFIG_FILE}"
+    fi
+    mkdir -p "${BACKUP_DIRECTORY}"
+    tar -cpzf "${BACKUP_FILENAME}.tmp" "${EXCLUSION_ARGUMENT}" "${PERSISTENT_VOLUMES_ROOT}"
+
+    mv "${BACKUP_FILENAME}.tmp" "${BACKUP_FILENAME}"
+    chown $BACKUP_UID:$BACKUP_GID "${BACKUP_FILENAME}"
+    echo "###### Finished Backing up persistent volumes ######"
+}
+
+remove_old_backups(){
+    echo "###### Removing old backups. Keeping last ${BACKUPS_TO_KEEP} ######"
+    #TODO change for find??
+    rm -f $(ls -1td ${BACKUP_DIRECTORY}/${BACKUP_NAME_PREFIX}* | tail -n +$((BACKUPS_TO_KEEP+1)))
+    echo "###### Finished removing old backups ######"
+}
+
+try_backup(){
+  get_deployment_names
+  stop_deployments
+  echo "Giving ${SLEEP_SECONDS} seconds to deployments to scale down"
+  sleep $SLEEP_SECONDS
+  backup
+  start_stopped_deployments
+  remove_old_backups
+  if [ "$notFound" -eq "0" ]; then
+     echo "Finished without warnings";
+  else
+     echo "Finished!! But there were $notFound directories without a deployment. Maybe they can be removed?"
+  fi
+}
+
+fallback() {
+  echo "Backup exited with error, will try to scale up deployments"
+  start_stopped_deployments
+}
+
+try_backup || fallback
+
+#TODO try to do incremental, so we can minimize time down per application
